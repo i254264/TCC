af856cd503569d4b3ce0bbf85e5698e3bd7c097b
Isabella Unicamp <i254264@dac.unicamp.br>
2026-09-19 14:43:13 -0300

    Revert "chosse model"
    This reverts commit 3915d9bd180376be5eafcaf51509446409877abc.

--- TopicGeneration/js/transition_pages.js
+++ TopicGeneration/js/transition_pages.js
@@ -6,28 +6,20 @@ window.addEventListener("DOMContentLoaded", function(){
     let transitionToCards = document.getElementById('lineToCards');
     let btnTutorial = document.getElementById('btnTutorial');
     
-    if (transitionToCards && panelCards) {
-        transitionToCards.onclick = () => {
-            panelCards.style.transform = `translateY(0)`;
-        };
-    }
-    if (btnTutorial && panel) {
-        btnTutorial.onclick = () => {
-            panel.style.transform = `translateY(0)`;
+    transitionToCards.onclick = () => {
+        panelCards.style.transform = `translateY(0)`;
+    };
+    btnTutorial.onclick = () => {
+        panel.style.transform = `translateY(0)`;
 
-            //adiciona a classe que vai animar a linha
-            if (lineAnimated) {
-                setTimeout(() => {
-                    lineAnimated.classList.add('animate-path-vert');
-                }, 1000);
-            }
-        }
+        //adiciona a classe que vai animar a linha
+        setTimeout(() => {
+            lineAnimated.classList.add('animate-path-vert');
+        }, 1000);
     }
 
     let btnCloseTutorial = document.getElementById('btnCloseTutorial');
-    if (btnCloseTutorial && panelCards) {
-        btnCloseTutorial.onclick = () => {
-            panelCards.style.transform = `translateY(100%)`;
-        }
+    btnCloseTutorial.onclick = () => {
+        panelCards.style.transform = `translateY(100%)`;
     }
 });
